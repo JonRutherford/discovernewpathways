3d0307232550e53a4d4e6fd3cedd9a25f6a42a38
Jon Rutherford <jr_rutherford@hotmail.com>
2013-06-13 19:38:11 +0930

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscoverNewPathways::Application.config.secret_token = '637291c7f49837df2e5ce03ea54721c89b2d68972b2ed45eda95ca0dcc461a163662b8ce9f3dcc25e7ad3113afa4da7b73ad05212059ed5f49f70b804b1051a0'
---
+++ db/schema.rb
@@ -0,0 +1,183 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130613094527) do
+
+  create_table "refinery_images", :force => true do |t|
+    t.string   "image_mime_type"
+    t.string   "image_name"
+    t.integer  "image_size"
+    t.integer  "image_width"
+    t.integer  "image_height"
+    t.string   "image_uid"
+    t.string   "image_ext"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "refinery_menus", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "permatitle"
+  end
+
+  add_index "refinery_menus", ["permatitle"], :name => "index_refinery_page_menus_on_permatitle", :unique => true
+
+  create_table "refinery_menus_links", :force => true do |t|
+    t.integer "parent_id"
+    t.integer "lft"
+    t.integer "rgt"
+    t.integer "depth"
+    t.integer "refinery_menu_id"
+    t.string  "menu_match"
+    t.integer "resource_id"
+    t.string  "resource_type"
+    t.string  "title_attribute"
+    t.string  "custom_url"
+    t.string  "label"
+    t.string  "id_attribute"
+    t.string  "class_attribute"
+  end
+
+  add_index "refinery_menus_links", ["depth"], :name => "index_refinery_page_positions_on_depth"
+  add_index "refinery_menus_links", ["id"], :name => "index_refinery_page_positions_on_id"
+  add_index "refinery_menus_links", ["lft"], :name => "index_refinery_page_positions_on_lft"
+  add_index "refinery_menus_links", ["parent_id"], :name => "index_refinery_page_positions_on_parent_id"
+  add_index "refinery_menus_links", ["rgt"], :name => "index_refinery_page_positions_on_rgt"
+
+  create_table "refinery_page_part_translations", :force => true do |t|
+    t.integer  "refinery_page_part_id"
+    t.string   "locale"
+    t.text     "body"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  add_index "refinery_page_part_translations", ["locale"], :name => "index_refinery_page_part_translations_on_locale"
+  add_index "refinery_page_part_translations", ["refinery_page_part_id"], :name => "index_refinery_page_part_translations_on_refinery_page_part_id"
+
+  create_table "refinery_page_parts", :force => true do |t|
+    t.integer  "refinery_page_id"
+    t.string   "title"
+    t.text     "body"
+    t.integer  "position"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "refinery_page_parts", ["id"], :name => "index_refinery_page_parts_on_id"
+  add_index "refinery_page_parts", ["refinery_page_id"], :name => "index_refinery_page_parts_on_refinery_page_id"
+
+  create_table "refinery_page_translations", :force => true do |t|
+    t.integer  "refinery_page_id"
+    t.string   "locale"
+    t.string   "title"
+    t.string   "custom_slug"
+    t.string   "menu_title"
+    t.string   "slug"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "refinery_page_translations", ["locale"], :name => "index_refinery_page_translations_on_locale"
+  add_index "refinery_page_translations", ["refinery_page_id"], :name => "index_refinery_page_translations_on_refinery_page_id"
+
+  create_table "refinery_pages", :force => true do |t|
+    t.integer  "parent_id"
+    t.string   "path"
+    t.string   "slug"
+    t.boolean  "show_in_menu",        :default => true
+    t.string   "link_url"
+    t.string   "menu_match"
+    t.boolean  "deletable",           :default => true
+    t.boolean  "draft",               :default => false
+    t.boolean  "skip_to_first_child", :default => false
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
+    t.string   "view_template"
+    t.string   "layout_template"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "refinery_pages", ["depth"], :name => "index_refinery_pages_on_depth"
+  add_index "refinery_pages", ["id"], :name => "index_refinery_pages_on_id"
+  add_index "refinery_pages", ["lft"], :name => "index_refinery_pages_on_lft"
+  add_index "refinery_pages", ["parent_id"], :name => "index_refinery_pages_on_parent_id"
+  add_index "refinery_pages", ["rgt"], :name => "index_refinery_pages_on_rgt"
+
+  create_table "refinery_resources", :force => true do |t|
+    t.string   "file_mime_type"
+    t.string   "file_name"
+    t.integer  "file_size"
+    t.string   "file_uid"
+    t.string   "file_ext"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "refinery_roles", :force => true do |t|
+    t.string "title"
+  end
+
+  create_table "refinery_roles_users", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "refinery_roles_users", ["role_id", "user_id"], :name => "index_refinery_roles_users_on_role_id_and_user_id"
+  add_index "refinery_roles_users", ["user_id", "role_id"], :name => "index_refinery_roles_users_on_user_id_and_role_id"
+
+  create_table "refinery_user_plugins", :force => true do |t|
+    t.integer "user_id"
+    t.string  "name"
+    t.integer "position"
+  end
+
+  add_index "refinery_user_plugins", ["name"], :name => "index_refinery_user_plugins_on_name"
+  add_index "refinery_user_plugins", ["user_id", "name"], :name => "index_refinery_user_plugins_on_user_id_and_name", :unique => true
+
+  create_table "refinery_users", :force => true do |t|
+    t.string   "username",               :null => false
+    t.string   "email",                  :null => false
+    t.string   "encrypted_password",     :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "sign_in_count"
+    t.datetime "remember_created_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
+  end
+
+  add_index "refinery_users", ["id"], :name => "index_refinery_users_on_id"
+
+  create_table "seo_meta", :force => true do |t|
+    t.integer  "seo_meta_id"
+    t.string   "seo_meta_type"
+    t.string   "browser_title"
+    t.string   "meta_keywords"
+    t.text     "meta_description"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
+  add_index "seo_meta", ["seo_meta_id", "seo_meta_type"], :name => "index_seo_meta_on_seo_meta_id_and_seo_meta_type"
+
+end
