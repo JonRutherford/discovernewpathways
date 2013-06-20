00ab4cedd04da7df5782a6458bfa75db9cc2e28c
Jon Rutherford <jr_rutherford@hotmail.com>
2013-06-20 20:55:53 +0930

    fuck this...

--- app/assets/javascripts/inquiry.js
+++ app/assets/javascripts/inquiry.js
@@ -1,15 +1,15 @@
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnD6iAFHLgTU9OYMsrKvzbkvNaRNB3Jf4&sensor=false">
-  </script>
+// <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnD6iAFHLgTU9OYMsrKvzbkvNaRNB3Jf4&sensor=false">
+//   </script>
 
-  <script type="text/javascript">
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-33.717415, 151.167755),
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
+//   <script type="text/javascript">
+//       function initialize() {
+//         var mapOptions = {
+//           center: new google.maps.LatLng(-33.717415, 151.167755),
+//           zoom: 16,
+//           mapTypeId: google.maps.MapTypeId.ROADMAP
+//         };
+//         var map = new google.maps.Map(document.getElementById("map_canvas"),
+//             mapOptions);
+//       }
+//       google.maps.event.addDomListener(window, 'load', initialize);
+//     </script>
